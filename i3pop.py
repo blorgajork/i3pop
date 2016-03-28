dba84f71c371dcb152e32eef12522497191bfa15
blorgajork <blorgajork@gmail.com>
2016-03-28 19:12:34 +0100

    Small formatting changes

--- i3pop.py
+++ i3pop.py
@@ -2,26 +2,28 @@
 
 import i3ipc, subprocess, argparse
 
+# Initialises the argument parser
 parser = argparse.ArgumentParser(description='Pops out a tiled window into a floating window in the same place')
 
+# Adds the --sticky option
 sticky_parser = parser.add_mutually_exclusive_group(required=False)
 sticky_parser.add_argument('--sticky', dest='stick', action='store_true')
 sticky_parser.set_defaults(stick=False)
-
 args = parser.parse_args()
+sticky = bool(args.stick)
 
 i3 = i3ipc.Connection()
 
 focused = i3.get_tree().find_focused()
 windowWidth, windowHeight = str(focused.rect.width), str(focused.rect.height)
-print(windowWidth + ' ' + windowHeight)
+#print(windowWidth + ' ' + windowHeight)
 windowX, windowY = str(focused.rect.x), str(focused.rect.y)
-print(windowX + ' ' + windowY)
+#print(windowX + ' ' + windowY)
 
-Float = subprocess.run(['i3-msg', 'floating', 'enable'])
-Move = subprocess.run(['i3-msg', 'move', 'absolute', 'position', windowX, windowY])
-Resize = subprocess.run(['i3-msg', 'resize', 'set', windowWidth, windowHeight])
+float_window = subprocess.run(['i3-msg', 'floating', 'enable'])
 
-sticky = bool(args.stick)
-if sticky == True: Stick = subprocess.run(['i3-msg', 'sticky', 'toggle'])
+move_window = subprocess.run(['i3-msg', 'move', 'absolute', 'position', windowX, windowY])
+resize_window = subprocess.run(['i3-msg', 'resize', 'set', windowWidth, windowHeight])
+
+if sticky == True: stick_window = subprocess.run(['i3-msg', 'sticky', 'toggle'])
 else:   pass
